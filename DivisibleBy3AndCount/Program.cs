755d6c65c04923a0ab6ba94dd8ee7a1c040573d8
17193 <asialevesquegault@gmail.com>
2021-01-13 19:48:12 -0700

    Add project files.

---
+++ DivisibleBy3AndCount/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DivisibleBy3AndCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0) // this solution implies 3/0.
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("There are {0} numbers % by 3 between 1-100....assumption being % by 0 counts as one",count);
+        }
+    }
+}
